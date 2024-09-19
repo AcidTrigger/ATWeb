16bbc92cc278ef8c438b314247d8027c1644b89b
Aaron <inbox.aaronsmith@gmail.com>
2024-09-19 16:33:02 +0100

    fixed twitch link and oak font size

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Open a link in a new window when icon4 is clicked
     document.getElementById('icon4').addEventListener('click', function() {
-        window.open('https://www.youtube.com/@AcidTrigger', '_blank'); // Replace with your desired URL
+        window.open('https://www.twitch.tv/acidtrigger', '_blank'); // Replace with your desired URL
     });
 
     // Show the modal when icon6 is clicked
